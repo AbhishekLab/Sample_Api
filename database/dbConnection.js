8f0fe7ea2d80a09bd823d93d36b34926eecf5126
Abhishek <abhi.cs005@gmail.com>
2021-01-15 12:52:28 +0530

    first commit

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const constant = require('../constant');
 
 //logic for the connectivity
 module.exports = async ()=>{
 //connect
 try {
-    await mongoose.connect('mongodb://localhost/apiDb', {useNewUrlParser: true, useUnifiedTopology: true})
+    await mongoose.connect("mongodb+srv://TheImortal:EDrQ25l40aKLBAgc@cluster0.uo2op.mongodb.net/Sample-Api?retryWrites=true&w=majority",
+     {useNewUrlParser: true, useUnifiedTopology: true})
     console.log('Database connectivity establish');
 } catch (error) {
     console.log('Database Connectivity Error',error);
