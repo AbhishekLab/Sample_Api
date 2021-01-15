dc8864812f27aa569d91058bb8cfb98fcae21f44
Abhishek <abhi.cs005@gmail.com>
2021-01-15 15:19:02 +0530

    misc

--- app.js
+++ app.js
@@ -32,11 +32,11 @@ app.use('/api/v1/user', usersRouter);
 
 // API Documentation
 if (process.env.NODE_ENV != 'production') {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
 
 app.get('/', (req, res) => {
-  res.redirect('/api-docs');
+  res.redirect('/');
 });
 
 // catch 404 and forward to error handler
