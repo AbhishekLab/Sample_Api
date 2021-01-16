7f7b60484bb30509d943de47d2bd6e3ead2973b5
Abhishek <abhi.cs005@gmail.com>
2021-01-16 09:36:37 +0530

    www removed

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var debug = require('debug')('node-generator-project:server');
+var http = require('http');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -12,6 +14,70 @@ const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml'); 
 
 var app = express();
+
+//creating port
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+var server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 //database connectivity
 dbConnection();
